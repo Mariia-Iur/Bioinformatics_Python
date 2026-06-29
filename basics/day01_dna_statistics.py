370c7f1136cfeed49440298b49ef9f70ef82ae03
Mariia-Iur <maur4ak2024@gmail.com>
2026-06-29 21:32:40 +0300

    Add DNA nucleotide counting script
    This script counts the occurrences of nucleotides A, T, G, and C in a given DNA sequence and prints the results.

---
+++ basics/day01_dna_statistics.py
@@ -0,0 +1,8 @@
+# Count nucleotides in a DNA sequence
+sequence = "ATTACGGCTAGGGCTTAGAATTCGGATCGTAGCTAGCTTAGGGACTTAGCTTAGCTTAGGCGCGATTATATCGGATC"
+print(sequence)
+print("Sequence lenght: ", len(sequence))
+print("A: ", sequence.count("A"))
+print("T: ", sequence.count("T"))
+print("G: ", sequence.count("G"))
+print("C: ", sequence.count("C"))
